6fb6504512e94e13d35a5ebd50a91f097db32ec6
Vanwuacha <vanwuacha@byupathway.edu>
2025-01-12 20:50:48 +0100

    Finished Exercise 2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,68 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter = "B";
+        }
+        else if (percent >= 70)
+        {
+            letter = "C";
+        }
+        else if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying! You can do better next time.");
+        }
+
+         string sign = "";
+        int lastDigit = percent % 10;
+
+        if (letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+
+            // Special case: No A+
+            if (letter == "A" && sign == "+")
+            {
+                sign = "";
+            }
+        }
+
+        // Step 5: Display the result
+        Console.WriteLine($"Your grade is: {letter}{sign}");
+    
     }
+
 }
